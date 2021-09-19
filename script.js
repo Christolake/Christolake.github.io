c0136ae4efa944fb8fbe902a3c96568cd5e5b4dc
Christolake <the.christolake@gmail.com>
2021-09-18 19:44:04 -0700

    init

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 const canvas = document.getElementById('canvas');
 //ctx = Context
 const ctx = canvas.getContext('2d');
-const CANVAS_WIDTH = canvas.width = 800;
-const CANVAS_HEIGHT = canvas.height = 600;
+const CANVAS_WIDTH = canvas.width = 3300;
+const CANVAS_HEIGHT = canvas.height = 400;
 
 const playerImage = new Image();
-playerImage.src = 'knight/Idle (1).png'
+playerImage.src = 'knight/KNIGHT.png'
 
 function animate (){
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
     ctx.fillRect(100, 50, 100, 100);
-    ctx.drawImage(playerImage, 0, 0);
+    ctx.drawImage(playerImage, 330, 400);
     requestAnimationFrame(animate);
 };
 animate();
