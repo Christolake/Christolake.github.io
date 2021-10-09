c4d8a389471376fea4edfda7ddc73013c41d19a7
Christolake <the.christolake@gmail.com>
2021-10-09 15:48:14 -0700

    inigt

--- learningApps/greenLight/app.js
+++ learningApps/greenLight/app.js
@@ -1,3 +1,9 @@
+
+const gameButton = document.querySelector('.game-state');
+const level = {
+    01: 5000, 02: 4000, 03: 3000, 04: 2000, 05: 1500
+};
+const min = 1000;
 const closedHead = document.querySelector('.closed');
 const openHead = document.querySelector('.open');
 const player = document.querySelector('.player');
@@ -5,17 +11,34 @@ const root = document.querySelector(':root');
 const dead = document.querySelector('.dead');
 let movement = document.documentElement;
 let clicks = 0;
-let transitionEnd = 
+let time = 0;
 
-closedHead.addEventListener("click", () => {
-    openHead.classList.add("active");
-    closedHead.classList.remove("active");
-});
+//EMPIEZA EL JUEGO. EL MONO HACE SU TRABAJO
 
-openHead.addEventListener("click", () => {
-    closedHead.classList.add("active");
+// function gameStart (theTime) {
+//     openHead.classList.remove("active");
+//     closedHead.classList.add("active")
+//     setTimeout(function resetInTime () {
+//         openHead.classList.remove("active");
+//         closedHead.classList.add("active");
+//     }, 1000);
+// }
+// PRESIONAR INICIAR JUEGO.
+
+gameButton.addEventListener('click', () => {
+    time = Math.ceil((Math.random() * (level[01] - min) + min));
+    gameButton.textContent = time + 'ms';
     openHead.classList.remove("active");
-});
+    closedHead.classList.add("active");
+    setTimeout(() => {
+        openHead.classList.add("active");
+        closedHead.classList.remove("active");
+     }, time);
+    }
+);
+
+
+//ACCIONES DEL JUGADOR
 
 player.addEventListener('click', () => {
     if (closedHead.classList.contains('active')) {
@@ -32,7 +55,8 @@ player.addEventListener('click', () => {
 player.addEventListener('transitionstart', () => {
     if (openHead.classList.contains('active')) {
     player.classList.remove('alive');
-    dead.classList.add('alive'); }
+    dead.classList.add('alive');
+    }
 })
 //player.addEventListener('webkitTransitionEnd', () => { console.log('transition end')}, false);
 
