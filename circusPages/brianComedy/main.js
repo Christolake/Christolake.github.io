16398de3d2b686a3e37aa94fc9aa9c1aeabfd51a
Christolake <the.christolake@gmail.com>
2021-11-13 23:36:12 -0800

    init

--- circusPages/brianComedy/main.js
+++ circusPages/brianComedy/main.js
@@ -1,5 +1,34 @@
 const comedy = new CircleType(document.getElementById('comedy'));
-const brianCircle = new CircleType(document.getElementById('logo'));
+const brian = new CircleType(document.getElementById('brian'));
+const bulbOn = document.querySelector('.bulbon');
+const bulbOff = document.querySelector('.bulboff');
+const bulb = document.querySelector('#toggle');
+const nav = document.querySelector('nav');
+const footer = document.querySelector('footer');
+const logoHero = document.querySelector('#logo-hero');
 
-brianCircle.radius(300);
+brian.radius(300);
 comedy.radius(700).dir(-1);
+
+function toggle() {
+  document.body.classList.toggle('dark');
+  nav.classList.toggle('dark-bg');
+  footer.classList.toggle('dark-bg');
+  footer.classList.toggle('dark-semitext');
+  logoHero.classList.toggle('dark-logo');
+
+}
+
+bulb.addEventListener('click', () => {
+    if (bulbOn.classList.contains('on')) {
+        bulbOn.classList.remove('on');
+        bulbOn.classList.add('off');
+        bulbOff.classList.add('on');
+        toggle();
+    } else {
+        bulbOff.classList.remove('on');
+        bulbOff.classList.add('off');
+        bulbOn.classList.add('on');
+        toggle();
+    }
+});
